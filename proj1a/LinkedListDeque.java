8020ab2e5554b6d28733982fba86acf13296c75e
Nvvvy <nvvvyus@NvvvydeMacBook-Pro.local>
2023-11-28 17:22:03 +0800

    Finished LinkedListDeque

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,17 +1,17 @@
-public class LinkedListDeque<ArbitraryType> {
+public class LinkedListDeque<T> {
 
     private class ListNode {
-        public ArbitraryType item;
-        public ListNode next;
-        public ListNode prev;
+        private T item;
+        private ListNode next;
+        private ListNode prev;
 
-        public ListNode(ArbitraryType i, ListNode n, ListNode p) {
+        public ListNode(T i, ListNode n, ListNode p) {
             item = i;
             next = n;
             prev = p;
         }
 
-        public ArbitraryType getRecursive(int i) {
+        public T getRecursive(int i) {
             if (i == 0) {
                 return item;
             }
@@ -19,14 +19,14 @@ public class LinkedListDeque<ArbitraryType> {
         }
     }
 
-    public ListNode sentinel = new ListNode((ArbitraryType) "sentinel", null, null);
-    public int size;
+    private ListNode sentinel = new ListNode((T) "sentinel", null, null);
+    private int size;
 
-    public LinkedListDeque(ArbitraryType x) {
-        sentinel.next = new ListNode(x, sentinel, sentinel);
-        sentinel.prev = sentinel.next;
-        size = 1;
-    }
+//    public LinkedListDeque(T x) {
+//        sentinel.next = new ListNode(x, sentinel, sentinel);
+//        sentinel.prev = sentinel.next;
+//        size = 1;
+//    }
 
     public LinkedListDeque() {
         sentinel.prev = sentinel;
@@ -34,13 +34,14 @@ public class LinkedListDeque<ArbitraryType> {
         size = 0;
     }
 
-    public void addFirst(ArbitraryType x) {
+    public void addFirst(T x) {
         sentinel.next = new ListNode(x, sentinel.next, sentinel);
         sentinel.next.prev = sentinel;
+        sentinel.next.next.prev = sentinel.next;
         size += 1;
     }
 
-    public void addLast(ArbitraryType x) {
+    public void addLast(T x) {
         sentinel.prev.next = new ListNode(x, sentinel, sentinel.prev);
         sentinel.prev = sentinel.prev.next;
         size += 1;
@@ -54,29 +55,29 @@ public class LinkedListDeque<ArbitraryType> {
         return sentinel.next == sentinel;
     }
 
-    public ArbitraryType removeFirst() {
+    public T removeFirst() {
         if (this.isEmpty()) {
             return null;
         }
-        ArbitraryType removed = sentinel.next.item;
+        T removed = sentinel.next.item;
         sentinel.next = sentinel.next.next;
         sentinel.next.prev = sentinel;
         size -= 1;
         return removed;
     }
 
-    public ArbitraryType removeLast() {
+    public T removeLast() {
         if (this.isEmpty()) {
             return null;
         }
-        ArbitraryType removed = sentinel.prev.item;
+        T removed = sentinel.prev.item;
         sentinel.prev = sentinel.prev.prev;
         sentinel.prev.next = sentinel;
         size -= 1;
         return removed;
     }
 
-    public ArbitraryType getRecursive(int index) {
+    public T getRecursive(int index) {
         ListNode p = sentinel.next;
         if (index == 0) {
             return p.item;
@@ -84,11 +85,15 @@ public class LinkedListDeque<ArbitraryType> {
         return p.next.getRecursive(index - 1);
     }
 
-    public ArbitraryType get(int index) {
+    public T get(int index) {
         ListNode p = sentinel.next;
         while (index > 0) {
-            p = p.next;
-            index -= 1;
+            if (p == sentinel) {
+                return null;
+            } else {
+                p = p.next;
+                index -= 1;
+            }
         }
         return p.item;
     }
@@ -102,15 +107,26 @@ public class LinkedListDeque<ArbitraryType> {
     }
 
     public static void main(String[] args) {
-        LinkedListDeque<String> l = new LinkedListDeque<>("hello");
-//        LinkedListDeque k = new LinkedListDeque();
-        l.addLast("world");
-        l.addFirst("v, ");
-        System.out.println(l.getRecursive(2));
-        String removed = l.removeFirst();
-        l.removeLast();
-        l.addLast("world!");
-        System.out.println(l.size());
-        l.printDeque();
+//        LinkedListDeque<String> l = new LinkedListDeque<>("hello");
+
+//        l.addLast("world");
+//        l.addFirst("v, ");
+//        System.out.println(l.getRecursive(2));
+//        String removed = l.removeFirst();
+//        l.removeLast();
+//        l.addLast("world!");
+//        System.out.println(l.size());
+//
+//        boolean r = l.isEmpty();
+//        l.printDeque();
+
+        LinkedListDeque k = new LinkedListDeque();
+        k.addFirst(0);
+        k.addFirst(1);
+        k.addFirst(2);
+        k.addFirst(3);
+        k.removeLast();
+        boolean rr = k.isEmpty();
+        k.printDeque();
     }
 }
