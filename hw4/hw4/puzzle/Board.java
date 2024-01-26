44b0e0fd5b531adf12d59d9fb29067e64c9d3c5c
Nvvvy <nvvvyus@NvvvydeMacBook-Pro.local>
2024-01-26 19:42:26 +0800

    v0.1

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -2,9 +2,54 @@ package hw4.puzzle;
 
 public class Board {
 
-    /** Returns the string representation of the board. 
+    /** Constructs a board from an N-by-N array of tiles where
+     tiles[i][j] = tile at row i, column j */
+    public Board(int[][] tiles) {
+
+    }
+
+    /** Returns value of tile at row i, column j (or 0 if blank) */
+    public int tileAt(int i, int j) {
+        return 0;
+    }
+
+    /** Returns the board size N */
+    public int size() {
+        return 0;
+    }
+
+    /** Returns the neighbors of the current board */
+    public Iterable<WorldState> neighbors() {
+        return null;
+    }
+
+    /** Hamming estimate described below */
+    public int hamming() {
+        return 0;
+    }
+
+    /** Manhattan estimate described below */
+    public int manhattan() {
+        return 0;
+    }
+
+    /** Estimated distance to goal. This method should
+     simply return the results of manhattan() when submitted to
+     Gradescope. */
+    public int estimatedDistanceToGoal() {
+        return 0;
+    }
+
+    /** Returns true if this board's tile values are the same
+     position as y's */
+    public boolean equals(Object y) {
+        return false;
+    }
+
+
+    /** Returns the string representation of the board.
       * Uncomment this method. */
-    /*public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +61,6 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
